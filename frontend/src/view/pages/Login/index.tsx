9e5824f6caa51d89dbb29ca3310cbba4a7c6c346
Luiz Almeida <146768585+Luiz-1213@users.noreply.github.com>
2025-03-18 13:52:30 -0300

    feat: create Login Page and controller for page
    Creation of the login page design.
    Abstraction of the page's operating logic in a Controller layer.
    Implementation of form validation using zod

---
+++ frontend/src/view/pages/Login/index.tsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+import { useLoginController } from './useLoginController';
+
+export function Login(){
+  const { handleSubmit, register, errors } = useLoginController();
+  return (
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold text-gray-700 tracking-[-1px]">
+       Entre em sua conta
+        </h1>
+        <p className="space-x-1">
+          <span className="text-gray-700 tracking-[-0.5px]">Novo por aqui?</span>
+          <Link to="/register" className=" font-medium text-teal-900 tracking-[-0.5px]">Crie uma conta</Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4" onSubmit={handleSubmit}>
+        <Input
+          type="email"
+          placeholder="Email"
+          error='informe um email'
+          {...register('email')}
+        />
+
+
+        <Input
+          type="password"
+          placeholder="Senha"
+          {...register('password')}
+        />
+
+        <Button type="submit" className="mt-2">
+          Entrar
+        </Button>
+      </form>
+    </>
+  );
+}
---
+++ frontend/src/view/pages/Login/useLoginController.tsx
@@ -0,0 +1,35 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const schema = z.object({
+  email: z
+    .string()
+    .min(1, 'E-mail é obrigatório')
+    .email('Informe um e-mail válido'),
+  password: z
+    .string()
+    .min(1, 'Senha é obrigatória')
+    .min(8, 'A senha deve conter pelo menos 8 dígitos'),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export function useLoginController() {
+  const {
+    register,
+    handleSubmit: hookFormSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const handleSubmit = hookFormSubmit((data)=>{
+    console.log({data});
+  });
+
+  return {handleSubmit, register, errors};
+}
+
+
+
