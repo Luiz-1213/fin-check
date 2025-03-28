2c5678a9ff6b9e7da2565b4c39f91fbcfbadbbac
Luiz Almeida <146768585+Luiz-1213@users.noreply.github.com>
2025-03-28 08:54:35 -0300

    feat: add account management components and hooks
    Created AccountCard component to display account details. Added useAccountsController hook for managing account-related state. Introduced SliderNavigation for better user interaction. Updated index.tsx for improved component structure.

---
+++ frontend/src/view/pages/Dashboard/components/Accounts/AccountCard.tsx
@@ -0,0 +1,30 @@
+
+import { cn } from '../../../../../app/utils/cn';
+import { formatCurrency } from '../../../../../app/utils/formatCurrency';
+import { BankAccountTypeIcon } from '../../../../components/icons/BankAccountTypeIcon';
+import { useDashboard } from '../DashboardContext/useDashboard';
+
+interface AccountCardProps{
+  name: string
+  balance: number
+  color: string
+  type: 'CASH'| 'CHECKING'| 'INVESTMENT'
+}
+
+export  function AccountCard({name, balance, color, type}: AccountCardProps) {
+  const {areValuesVisible} = useDashboard();
+  return (
+    <div
+      className="p-4 bg-white rounded-2xl h-[200px] flex flex-col justify-between border-b-4 border-teal-950"
+      style={{borderColor: color}}>
+      <div>
+        <BankAccountTypeIcon type={type}/>
+        <span className='text-gray-800 font-medium tracking-[-0.5px] mt-4 block'>{name}</span>
+      </div>
+      <div>
+        <span className={cn('text-gray-800 font-medium tracking-[-0.5px] block', !areValuesVisible && 'blur-sm')}>{formatCurrency(balance)}</span>
+        <small className=' text-gray-600 text-sm'>Saldo Atual</small>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/view/pages/Dashboard/components/Accounts/index.tsx
@@ -0,0 +1,133 @@
+
+import 'swiper/css';
+import { EyeIcon } from '../../../../components/icons/EyeIcon';
+import { AccountCard } from './AccountCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+
+import { SliderNavigation } from './SliderNavigation';
+import { useAccountsController } from './useAccountsController';
+import { formatCurrency } from '../../../../../app/utils/formatCurrency';
+import { cn } from '../../../../../app/utils/cn';
+import { Spinner } from '../../../../components/Spinner';
+import { PlusIcon } from '@radix-ui/react-icons';
+
+export  function Accounts() {
+  const {
+    windowWidth,
+    sliderState,
+    setSliderState,
+    areValuesVisible,
+    toggleValuesVisibility,
+    isLoading,
+    accounts
+  } = useAccountsController();
+
+
+  return (
+    <div className="bg-teal-900 rounded-2xl w-full h-full px-4 py-8 md:p-10 flex flex-col">
+      {isLoading && (
+        <div className='w-full h-full flex items-center justify-center'>
+          <Spinner className='text-teal-950/50 fill-white h-10 w-10'/>
+        </div>
+      )}
+
+      {!isLoading && (
+        <>
+          <div>
+            <span className=" tracking-[-0.5px] text-white block">Saldo total</span>
+            <div className='flex items-center gap-2'>
+              <strong className={cn('text-2xl tracking-[-1px] text-white',!areValuesVisible && 'blur-md' )}>
+                {formatCurrency(1000.00)}
+              </strong>
+              <button
+                className='flex items-center justify-center w-8 h-8'
+                onClick={toggleValuesVisibility}>
+                <EyeIcon open={!areValuesVisible}/>
+              </button>
+            </div>
+          </div>
+          <div className="flex-1 flex flex-col justify-end mt-10 md:mt-0">
+            {accounts.length === 0 && (
+              <>
+                <div
+                  className="mb-4"
+                >
+                  <strong className="text-white tracking-[-1px] text-lg">
+                   Minhas contas
+                  </strong>
+                </div>
+
+                <button
+                  className='mt-4 h-52 rounded-2xl border-2 border-dashed border-teal-600 flex flex-col items-center justify-center gap-4 text-white'
+                >
+                  <div className='w-11 h-11 rounded-full border-2 border-dashed border-white flex  items-center justify-center'>
+                    <PlusIcon className='w-6 h-6'/>
+                  </div>
+                  <span className='font-medium tracking-[-0.5px] block w-32 text-center'>Cadastre uma nova conta</span>
+                </button>
+              </>
+
+            )}
+
+            {accounts.length > 0 &&(
+              <div>
+                <Swiper
+                  spaceBetween={16}
+                  slidesPerView={windowWidth >= 500 ? 2.1 : 1.1}
+                  onSlideChange={swiper =>{
+                    setSliderState({
+                      isBeginning: swiper.isBeginning,
+                      isEnd: swiper.isEnd
+                    });
+                  }}
+                >
+                  <div
+                    className="flex items-center justify-between mb-4"
+                    slot="container-start"
+                  >
+                    <strong className="text-white tracking-[-1px] text-lg">
+                Minhas contas
+                    </strong>
+
+                    <SliderNavigation
+                      isBeginning={sliderState.isBeginning}
+                      isEnd={sliderState.isEnd}
+                    />
+                  </div>
+
+                  <SwiperSlide>
+                    <AccountCard
+                      name={'Nubank'}
+                      balance={1056.93}
+                      color={'#7950f2'}
+                      type='CHECKING' />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <AccountCard
+                      name={'XP'}
+                      balance={500.64}
+                      color={'#333'}
+                      type='INVESTMENT' />
+                  </SwiperSlide>
+
+                  <SwiperSlide>
+                    <AccountCard
+                      name={'Carteira'}
+                      balance={20.50}
+                      color={'#0f0'}
+                      type='CASH' />
+                  </SwiperSlide>
+                </Swiper>
+              </div>)}
+
+          </div>
+        </>)}
+    </div>
+  );
+}
+
+
+
