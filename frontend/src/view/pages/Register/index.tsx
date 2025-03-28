225253728efa67de2dffcd2888331ba72e8e180d
Luiz Almeida <146768585+Luiz-1213@users.noreply.github.com>
2025-03-28 08:52:01 -0300

    feat: create RegisterPage and Controller

--- frontend/src/view/pages/Register/index.tsx
+++ frontend/src/view/pages/Register/index.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
+import { useRegisterController } from './useRegisterController';
 
 export function Register(){
+  const {errors, register, handleSubmit, isLoading} = useRegisterController();
   return (
     <>
       <header className="flex flex-col items-center gap-4 text-center">
@@ -15,12 +17,31 @@ export function Register(){
         </p>
       </header>
 
-      <form className='mt-[60px] flex flex-col gap-4' >
-        <Input name='name'  placeholder='Nome'/>
-        <Input name='email' type='email' placeholder='Email'/>
-        <Input name='password' type="password" placeholder='Senha'/>
+      <form
+        onSubmit={handleSubmit}
+        className='mt-[60px] flex flex-col gap-4'
+      >
+        <Input
+          placeholder='Nome'
+          {...register('name')}
+          error={errors.name?.message}/>
+        <Input
+          type='email'
+          placeholder='Email'
+          {...register('email')}
+          error={errors.email?.message}/>
+        <Input
+          type="password"
+          placeholder='Senha'
+          {...register('password')}
+          error={errors.password?.message}/>
 
-        <Button type="submit" className='mt-2'>Criar conta</Button>
+        <Button
+          type="submit"
+          className='mt-2'
+          isLoading={isLoading}>
+          Criar conta
+        </Button>
       </form>
     </>
   );
---
+++ frontend/src/view/pages/Register/useRegisterController.ts
@@ -0,0 +1,59 @@
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import { authService } from '../../../app/services/authService';
+import { SignupParams } from '../../../app/services/authService/signup';
+import { toast } from 'react-hot-toast';
+import { useAuth } from '../../../app/hooks/useAuth';
+
+const schema = z.object({
+  name: z.string().min(1, 'Nome é obrigatório'),
+  email: z
+    .string()
+    .min(1, 'E-mail é obrigatório')
+    .email('Informe um e-mail válido'),
+  password: z
+    .string()
+    .min(1, 'Senha é obrigatória')
+    .min(8, 'A senha deve conter pelo menos 8 dígitos'),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export function useRegisterController() {
+  const {
+    register,
+    handleSubmit: hookFormSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const { isLoading, mutateAsync } = useMutation({
+    mutationFn: async (data: SignupParams) => {
+      return authService.signup(data);
+    },
+  });
+
+  const { signin } = useAuth();
+
+  const handleSubmit = hookFormSubmit(async (data) => {
+    try {
+      const { accessToken } = await mutateAsync(data);
+
+      signin(accessToken);
+
+      toast.success('Conta criada com sucesso!');
+    } catch {
+      toast.error('Ocorreu um erro ao criar a sua conta!');
+    }
+  });
+
+  return {
+    handleSubmit,
+    register,
+    errors,
+    isLoading,
+  };
+}
