4c17fdf413c13da71c92d08ed39de3942ec4c668
Luiz Almeida <146768585+Luiz-1213@users.noreply.github.com>
2025-03-28 08:32:12 -0300

    feat: implement AuthContext and Service
    Create AuthContext to control if the user is authenticated or not.
    Implement signIn and signOut functionality.
    Create a custom hook useAuth to use AuthContext in the application.
    Create a service function for signIn and signUp.

---
+++ frontend/src/app/contexts/AuthContext.tsx
@@ -0,0 +1,74 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import {
+  ReactNode,
+  createContext,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
+import toast from 'react-hot-toast';
+import { localStorageKeys } from '../config/localStorageKeys';
+import { usersService } from '../services/usersService';
+import { LaunchScreen } from '../../view/components/LaunchScreen';
+
+
+interface AuthContextValue {
+  signedIn: boolean;
+  signin(accessToken: string): void;
+  signout(): void;
+}
+
+export const AuthContext = createContext({} as AuthContextValue);
+
+export  function AuthProvider({ children }: { children: ReactNode }) {
+  const [signedIn, setSignedIn] = useState<boolean>(() => {
+    const storedAccessToken = localStorage.getItem(
+      localStorageKeys.ACCESS_TOKEN
+    );
+
+    return !!storedAccessToken;
+  });
+
+  const queryClient = useQueryClient();
+
+  const { isError, isFetching, isSuccess,  } = useQuery({
+    queryKey: ['users', 'me'],
+    queryFn: () => usersService.me(),
+    enabled: signedIn,
+    staleTime: Infinity,
+  });
+
+  const signin = useCallback((accessToken: string) => {
+    localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
+    setSignedIn(true);
+  }, []);
+
+  const signout = useCallback(() => {
+    localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
+
+    queryClient.removeQueries({ queryKey: ['users', 'me'] });
+
+    setSignedIn(false);
+  }, [queryClient]);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error('Sua sessão expirou!');
+      signout();
+    }
+  }, [isError, signout]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        signedIn: isSuccess && signedIn,
+        signin,
+        signout,
+      }}
+    >
+      <LaunchScreen isLoading={isFetching}/>
+
+      {!isFetching && children}
+    </AuthContext.Provider>
+  );
+}
