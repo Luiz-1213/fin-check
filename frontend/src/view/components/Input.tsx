01386beff94ab64ac1949de2ae7722381a52f4dd
Luiz Almeida <146768585+Luiz-1213@users.noreply.github.com>
2025-03-28 08:50:51 -0300

    feat: create and update UI components
    Created DropdownMenu, LaunchScreen, Modal, Spinner, and UserMenu components. Updated Button and Input components to enhance UI consistency and user experience.

--- frontend/src/view/components/Input.tsx
+++ frontend/src/view/components/Input.tsx
@@ -1,5 +1,6 @@
 import {CrossCircledIcon} from'@radix-ui/react-icons';
 import { ComponentProps, forwardRef } from 'react';
+import { cn } from '../../app/utils/cn';
 
 
 interface InputProps extends ComponentProps<'input'> {
@@ -8,7 +9,7 @@ interface InputProps extends ComponentProps<'input'> {
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ placeholder, name, id, error, ...props }, ref) => {
+  ({ placeholder, name, id, error, className, ...props }, ref) => {
     const inputId = id ?? name;
 
     return (
@@ -19,7 +20,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
           name={name}
           id={inputId}
           placeholder=" "
-          className='bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800 pt-4 peer placeholder-shown:pt-0 focus:border-gray-800 transition-all outline-none'
+          className={cn('bg-white w-full rounded-lg border border-gray-500 px-3 h-[52px] text-gray-800 pt-4 peer placeholder-shown:pt-0 focus:border-gray-800 transition-all outline-none', error && '!border-red-900', className)}
         />
 
         <label
