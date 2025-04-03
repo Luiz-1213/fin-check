7e40bd57e8354bd8739d62132d7475aebc5bb61b
Luiz Almeida <146768585+Luiz-1213@users.noreply.github.com>
2025-04-03 08:35:24 -0300

    feat: implements logic to close Modal

--- frontend/src/view/components/Modal.tsx
+++ frontend/src/view/components/Modal.tsx
@@ -1,4 +1,4 @@
-import * as Dialog from '@radix-ui/react-dialog';
+import * as RdxDialog from '@radix-ui/react-dialog';
 import { cn } from '../../app/utils/cn';
 import { Cross2Icon } from '@radix-ui/react-icons';
 
@@ -6,40 +6,53 @@ import { Cross2Icon } from '@radix-ui/react-icons';
 interface ModalProps{
   open: boolean
   title: string
+  onClose?(): void
   rightAction?: React.ReactNode
   children: React.ReactNode
 }
 
-export function Modal({open, children, rightAction,  title}: ModalProps) {
+export function Modal({
+  open,
+  children,
+  rightAction,
+  onClose,
+  title}: ModalProps) {
   return (
-    <Dialog.Root open={open}>
-      <Dialog.Portal>
-        <Dialog.Overlay className={cn(
+    <RdxDialog.Root open={open} onOpenChange={onClose}>
+      <RdxDialog.Portal>
+        <RdxDialog.Overlay className={cn(
           'fixed inset-0 z-40 bg-black/80 backdrop-blur-sm',
           'data-[state=open]:animate-overlay-show'
         )} />
-        <Dialog.Content className={cn(
+        <RdxDialog.Content className={cn(
           'fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 p-6 bg-white space-y-10 rounded-2xl z-50 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] outline-none',
           'data-[state=open]:animate-content-show w-full max-w-[400px]'
-        )}>
-          <header className='h-12 flex items-center justify-between text-gray-800'>
-            <button className='w-12 h-12 flex items-center justify-center outline-none'>
-              <Cross2Icon className='w-6 h-6'/>
-            </button>
-            <span className='text-lg tracking-[-1px] font-bold'>
-              {title}
-            </span>
+        )}
+        aria-describedby={undefined}
+        >
+          <RdxDialog.Title>
+            <header className='h-12 flex items-center justify-between text-gray-800'>
+              <button
+                className='w-12 h-12 flex items-center justify-center outline-none'
+                onClick={onClose}>
+                <Cross2Icon className='w-6 h-6'/>
+              </button>
+              <span className='text-lg tracking-[-1px] font-bold'>
+                {title}
+              </span>
 
-            <div className='w-12 h-12 flex items-center justify-center'>
-              {rightAction}
-            </div>
-          </header>
+              <div className='w-12 h-12 flex items-center justify-center'>
+                {rightAction}
+              </div>
+            </header>
+          </RdxDialog.Title>
+
           <div>
             {children}
           </div>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+        </RdxDialog.Content>
+      </RdxDialog.Portal>
+    </RdxDialog.Root>
   );
 }
 
