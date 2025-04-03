fabc2de6efea8fa264cdf11ef48b81129c178694
Luiz Almeida <146768585+Luiz-1213@users.noreply.github.com>
2025-04-03 08:25:57 -0300

    feat: Create new Transaction Modal
    Creating a component to handle the creation of a new transaction, whether it is an expense or an income transaction.
    Separating the logic and UI into a controller component

---
+++ frontend/src/view/pages/Dashboard/modals/NewTransactionModal/index.tsx
@@ -0,0 +1,81 @@
+import { Button } from '../../../../components/Button';
+import { DatePickerInput } from '../../../../components/DatePickerInput';
+import { Input } from '../../../../components/Input';
+import { InputCurrency } from '../../../../components/InputCurrency';
+import { Modal } from '../../../../components/Modal';
+import { Select } from '../../../../components/Select';
+import { useNewTransactionModalController } from './useNewTransactionModalController';
+
+
+export  function NewTransactionModal() {
+  const {
+    closeNewTransactionModal,
+    isNewTransactionModalOpen,
+    newTransactionType
+  } = useNewTransactionModalController();
+
+  const isExpense = newTransactionType === 'EXPENSE';
+
+
+  return (
+    <Modal
+      title={ isExpense ? 'Nova Despesa' : 'Nova Receita'}
+      open={isNewTransactionModalOpen}
+      onClose={closeNewTransactionModal}>
+      <form>
+        <div>
+          <span className='text-gray-600 text-xs tracking-[-0.5px]'>Valor da {isExpense ? 'Despesa' : 'Receita'}</span>
+          <div className='flex gap-2 items-center'>
+            <span className='text-gray-600 text-lg tracking-[-0.5px]'>R$</span>
+            <InputCurrency/>
+          </div>
+        </div>
+        <div className='mt-10 flex flex-col gap-4'>
+          <Input
+            type='text'
+            name='name'
+            placeholder={isExpense ? 'Nome da Despesa' : 'Nome da Receita'}
+          />
+          <Select
+            placeholder={'Categoria'}
+            options={[
+              {
+                value: 'CHECKING',
+                label: 'Conta Corrente'
+              },
+              {
+                value: 'INVESTIMENT',
+                label: 'Investimentos'
+              },
+              {
+                value: 'CASH',
+                label: 'Dinheiro Físico'
+              },
+            ]}>
+
+          </Select>
+          <Select
+            placeholder={isExpense ? 'Pagar com' : 'Receber com '}
+            options={[
+              {
+                value: 'CHECKING',
+                label: 'Conta Corrente'
+              },
+              {
+                value: 'INVESTIMENT',
+                label: 'Investimentos'
+              },
+              {
+                value: 'CASH',
+                label: 'Dinheiro Físico'
+              },
+            ]}>
+          </Select>
+          <DatePickerInput/>
+        </div>
+        <Button type='submit' className='w-full mt-6'>Criar</Button>
+      </form>
+
+    </Modal>
+  );
+}
