1eb9e4fabfd6d185765b7ffa8333ba10348e149b
Luiz Almeida <luizalmeida090101@gmail.com>
2025-07-22 10:48:11 -0300

    fix: numeric format

--- frontend/src/view/components/InputCurrency.tsx
+++ frontend/src/view/components/InputCurrency.tsx
@@ -1,36 +1,36 @@
 import { CrossCircledIcon } from '@radix-ui/react-icons';
-import {NumericFormat} from 'react-number-format';
+import { NumericFormat } from 'react-number-format';
 import { cn } from '../../app/utils/cn';
 
-interface InputCurrencyProps{
-  error?: string
-  value?: string | number
-  onChange?(value: string):void
-
+interface InputCurrencyProps {
+  error?: string;
+  value?: string | number;
+  onChange?(value: string): void;
 }
 
-export function InputCurrency({error, onChange, value}: InputCurrencyProps) {
+export function InputCurrency({ error, onChange, value }: InputCurrencyProps) {
   return (
     <div>
       <NumericFormat
-        thousandSeparator='.'
-        decimalSeparator=','
+        placeholder="0,00"
+        thousandSeparator="."
+        decimalSeparator=","
         decimalScale={2}
-        fixedDecimalScale={true}
+        fixedDecimalScale={false}
         value={value}
-        onChange={event => onChange?.(event.target.value)}
-        className={cn(' w-full text-gray-800 text-[32px] font-bold tracking-[-1px] outline-none', error && 'text-red-900')}
+        onChange={(event) => onChange?.(event.target.value)}
+        className={cn(
+          ' w-full text-gray-800 text-[32px] font-bold tracking-[-1px] outline-none',
+          error && 'text-red-900'
+        )}
       />
 
       {error && (
-        <div className='flex gap-2 items-center mt-2 text-red-900'>
-          <CrossCircledIcon/>
-          <span className=' text-xs'>{error}</span>
-        </div>)
-      }
-
+        <div className="flex gap-2 items-center mt-2 text-red-900">
+          <CrossCircledIcon />
+          <span className=" text-xs">{error}</span>
+        </div>
+      )}
     </div>
   );
 }
-
-
