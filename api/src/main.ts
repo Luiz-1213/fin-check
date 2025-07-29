1a45afd4aad9c651ee13e4dc3077cefae65cb500
Luiz Almeida <146768585+luiz-almeida-dev@users.noreply.github.com>
2025-07-29 14:02:46 -0300

    feat: add Swagger setup to main.ts with JWT bearer authentication

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,18 +1,30 @@
 import 'dotenv/config';
 
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  // arrumar para a url no deploy
+
   app.enableCors({
     origin: '*',
   });
 
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Fincheck API')
+      .setDescription('Documentação da API do Fincheck')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
+  }
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
