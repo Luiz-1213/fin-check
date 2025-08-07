b1f694c9f33844152d8e5f498587893130cd042f
Luiz-1213 <146768585+luiz-almeida-dev@users.noreply.github.com>
2025-08-07 11:56:43 -0300

    feat(ui): revoke refresh token on sign out

--- frontend/src/view/components/UserMenu.tsx
+++ frontend/src/view/components/UserMenu.tsx
@@ -1,29 +1,29 @@
 import { ExitIcon } from '@radix-ui/react-icons';
-import { DropdownMenu } from './DropdownMenu';
 import { useAuth } from '../../app/hooks/useAuth';
+import { DropdownMenu } from './DropdownMenu';
 
+export function UserMenu() {
+  const { signout, user } = useAuth();
 
-export  function UserMenu() {
-  const {signout, user } = useAuth();
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
-        <button className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100" >
-          <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">{user?.name.slice(0,2).toUpperCase()}</span>
+        <button className="bg-teal-50 rounded-full w-12 h-12 flex items-center justify-center border border-teal-100">
+          <span className="text-sm tracking-[-0.5px] text-teal-900 font-medium">
+            {user?.name.slice(0, 2).toUpperCase()}
+          </span>
         </button>
       </DropdownMenu.Trigger>
 
-      <DropdownMenu.Content className='w-32'>
+      <DropdownMenu.Content className="w-32">
         <DropdownMenu.Item
-          className='flex items-center justify-between'
+          className="flex items-center justify-between"
           onSelect={signout}
         >
           Sair
-          <ExitIcon className='w-4 h-4'></ExitIcon>
+          <ExitIcon className="w-4 h-4"></ExitIcon>
         </DropdownMenu.Item>
       </DropdownMenu.Content>
-
     </DropdownMenu.Root>
-
   );
 }
