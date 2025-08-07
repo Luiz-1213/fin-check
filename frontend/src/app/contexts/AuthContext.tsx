722b4a52fc548918e1bd18b5eed807da23a632b8
Luiz-1213 <146768585+luiz-almeida-dev@users.noreply.github.com>
2025-08-07 11:55:26 -0300

    feat(auth): implement silent refresh on 401 responses
    Added logic in `AuthContext` to intercept 401 responses using Axios interceptors.
    If an access token is expired, the app will attempt a silent refresh using the stored refresh token.
    
    Used `useLayoutEffect` to register the interceptor at app start.

--- frontend/src/app/contexts/AuthContext.tsx
+++ frontend/src/app/contexts/AuthContext.tsx
@@ -4,54 +4,101 @@ import {
   createContext,
   useCallback,
   useEffect,
+  useLayoutEffect,
   useState,
 } from 'react';
 import toast from 'react-hot-toast';
-import { localStorageKeys } from '../config/localStorageKeys';
-import { usersService } from '../services/usersService';
 import { LaunchScreen } from '../../view/components/LaunchScreen';
+import { localStorageKeys } from '../config/localStorageKeys';
 import { User } from '../entities/User';
+import { authService } from '../services/authService';
+import { httpClient } from '../services/httpClient';
+import { usersService } from '../services/usersService';
 
+interface ISignParams {
+  accessToken: string;
+  refreshToken: string;
+}
 
 interface AuthContextValue {
   signedIn: boolean;
-  signin(accessToken: string): void;
+  signin(signParams: ISignParams): void;
   signout(): void;
   user: User | undefined;
 }
 
 export const AuthContext = createContext({} as AuthContextValue);
 
-export  function AuthProvider({ children }: { children: ReactNode }) {
+export function AuthProvider({ children }: { children: ReactNode }) {
   const [signedIn, setSignedIn] = useState<boolean>(() => {
-    const storedAccessToken = localStorage.getItem(
-      localStorageKeys.ACCESS_TOKEN
+    const storedRefreshToken = localStorage.getItem(
+      localStorageKeys.REFRESH_TOKEN
     );
 
-    return !!storedAccessToken;
+    return !!storedRefreshToken;
   });
 
   const queryClient = useQueryClient();
 
-  const { isError, isFetching, isSuccess,  data  } = useQuery({
+  const { isError, isFetching, isSuccess, data } = useQuery({
     queryKey: ['users', 'me'],
     queryFn: () => usersService.me(),
     enabled: signedIn,
     staleTime: Infinity,
   });
 
-  const signin = useCallback((accessToken: string) => {
+  const signin = useCallback(({ accessToken, refreshToken }: ISignParams) => {
     localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
+    localStorage.setItem(localStorageKeys.REFRESH_TOKEN, refreshToken);
     setSignedIn(true);
   }, []);
 
-  const signout = useCallback(() => {
+  const signout = useCallback(async () => {
+    const refreshToken = localStorage.getItem(localStorageKeys.REFRESH_TOKEN);
+
+    await authService.revokeToken(refreshToken!);
+
     localStorage.removeItem(localStorageKeys.ACCESS_TOKEN);
+    localStorage.removeItem(localStorageKeys.REFRESH_TOKEN);
 
     queryClient.removeQueries({ queryKey: ['users', 'me'] });
 
     setSignedIn(false);
   }, [queryClient]);
+
+  useLayoutEffect(() => {
+    const interceptorId = httpClient.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config;
+        const refreshToken = localStorage.getItem(
+          localStorageKeys.REFRESH_TOKEN
+        );
+
+        if (originalRequest.url === 'refresh-token') {
+          setSignedIn(false);
+          localStorage.clear();
+          return Promise.reject(error);
+        }
+
+        if (error.response?.status !== 401 || !refreshToken) {
+          return Promise.reject(error);
+        }
+
+        const { accessToken, refreshToken: newRefreshToken } =
+          await authService.refreshToken(refreshToken);
+
+        localStorage.setItem(localStorageKeys.ACCESS_TOKEN, accessToken);
+        localStorage.setItem(localStorageKeys.REFRESH_TOKEN, newRefreshToken);
+
+        return httpClient(originalRequest);
+      }
+    );
+
+    return () => {
+      httpClient.interceptors.response.eject(interceptorId);
+    };
+  }, []);
 
   useEffect(() => {
     if (isError) {
@@ -66,10 +113,10 @@ export  function AuthProvider({ children }: { children: ReactNode }) {
         signedIn: isSuccess && signedIn,
         signin,
         signout,
-        user: data
+        user: data,
       }}
     >
-      <LaunchScreen isLoading={isFetching}/>
+      <LaunchScreen isLoading={isFetching} />
 
       {!isFetching && children}
     </AuthContext.Provider>
